8de421e96ff4e3ec22934a5091d415016e802192
Felipe Leal <felipe.leal.sierra@gmail.com>
2020-02-07 17:37:17 -0700

    Initial Commit

---
+++ itHelper2.0Tests/itHelper2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  itHelper2_0Tests.swift
+//  itHelper2.0Tests
+//
+//  Created by Felipe on 2/7/20.
+//  Copyright © 2020 division. All rights reserved.
+//
+
+import XCTest
+@testable import itHelper2_0
+
+class itHelper2_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
